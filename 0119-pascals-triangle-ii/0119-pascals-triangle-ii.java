cc3449c4a6e82d00842e6a9cee07cb733812eddb
Gaurang Chouhan <gaurangchouhan316@gmail.com>
2026-07-17 15:49:36 +0530

    Time: 0 ms (100%), Space: 42.7 MB (36.35%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> list = new ArrayList<>();
+
+        long ans = 1;
+        list.add((int)ans);
+
+        for(int i = 1; i<=rowIndex; i++){
+            ans = ans*(rowIndex-i +1)/i;
+            list.add((int)ans);
+        }
+        return list;
+    }
+}
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
