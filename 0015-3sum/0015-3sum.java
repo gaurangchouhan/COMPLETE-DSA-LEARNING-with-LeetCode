e48eb137c13c49009e0c3ce3c2d2c9ffdea721f3
Gaurang Chouhan <gaurangchouhan316@gmail.com>
2026-07-19 15:48:47 +0530

    Time: 1997 ms (5%), Space: 55.4 MB (97.8%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] num) {
+        int n = num.length;
+        Set<List<Integer>> st = new HashSet<>();
+
+        for (int i = 0; i < n; i++) { // near abt O(n)
+            Set<Integer> hashset = new HashSet<>();
+
+            for (int j = i + 1; j < n; j++) {  // near abt O(n)
+                int third = -(num[i] + num[j]);
+
+                if (hashset.contains(third)) { // near abt O(logM) 
+                    List<Integer> temp = Arrays.asList(num[i], num[j], third);
+                    Collections.sort(temp);
+                    st.add(new ArrayList<>(temp));
+                }
+
+                hashset.add(num[j]);
+            }
+        }
+
+        return new ArrayList<>(st);
+    }
+}
+
+// Time Complexity: O(n^2)   *    O(logM)
+//                 two for        hashmap
+//                 loop          (M - variable)
+
+// Space complexity: O(N) (N-number of elements)
