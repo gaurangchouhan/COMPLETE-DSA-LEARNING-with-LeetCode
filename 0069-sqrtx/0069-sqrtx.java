fc25f0ba6c4550d28ca6d2bb9a3d2c98b9dc3cc8
Gaurang Chouhan <gaurangchouhan316@gmail.com>
2026-08-17 20:28:15 +0530

    Time: 1 ms (98.15%), Space: 42.4 MB (79.44%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,20 @@
+ class Solution {
+    public int mySqrt(int x) {
+        if (x == 0 || x == 1) {
+            return x;
+        }
+        int low = 1;
+        int high = x;
+        while (low <= high) {
+            int mid = low + (high - low)/2;
+            if ((long)mid*mid == (long)x) {
+                return mid;
+            } else if ((long)mid*mid < (long)x) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return Math.round(high);
+    }
+}
