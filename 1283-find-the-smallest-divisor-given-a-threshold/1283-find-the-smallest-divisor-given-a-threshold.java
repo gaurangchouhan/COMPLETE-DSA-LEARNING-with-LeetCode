f76bcdabcd51e70aedb52641ccc49d481fbd7453
Gaurang Chouhan <gaurangchouhan316@gmail.com>
2026-08-21 14:38:47 +0530

    Time: 29 ms (33.59%), Space: 51 MB (74.55%) - LeetHub

---
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int smallestDivisor(int[] a, int t) {
+        int n = a.length;
+        int min = 1;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            if(max < a[i]){
+                max = a[i];
+            }
+        }
+
+        int ans = max;
+
+        while(min <= max){
+            int div = 0;
+            int mid = min + (max - min)/2;
+
+            for(int i=0; i<n; i++){
+                div += (int) Math.ceil((double) a[i] /mid);
+            }
+            // System.out.println("div " + mid + ":"+ div); debuging
+
+            if(div <= t){
+                ans = Math.min(ans, mid);
+                max = mid - 1;
+            }else{
+                min = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+}
+
+// Concept: Binary Search
+// Time complexity: O(n)      +       O(logM)
+//                 For loop         binary search 
+//            inside while loop   (M: the maximum element in the array)
+
+// Space complexity: O(1)
